aa5ae071e798d8af0fa215fa78bc925e6b66b83a
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-21 22:15:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,9 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  
+  array.each do |array|
+   puts "#{array.capitalize} wants to start a union!"
+  end  
 end
 
 def list_dwarves(array)
